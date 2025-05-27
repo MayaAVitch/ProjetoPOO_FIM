2910aed07ea028242d0e68200b0f300c53ba264a
Maya.Vitch <81265584+MayaAVitch@users.noreply.github.com>
2025-05-27 14:32:33 -0300

    Update Tomate.java

--- src/Infos/SubProdutos/Tomate.java
+++ src/Infos/SubProdutos/Tomate.java
@@ -1,4 +1,12 @@
 package Infos.SubProdutos;
 
-public class Tomate {
+import Infos.Produto;
+
+public class Tomate extends Produto {
+    public Tomate(String nome, int quantidade, double preco){super(nome, quantidade, preco);}
+
+    @Override
+    public void exibirInformacoes(){
+        System.out.println("O produto " + super.nome + " tem um preço de R$" + super.preco + " e uma quantidade de " + super.quantidade + " no estoque.");
+    }
 }
