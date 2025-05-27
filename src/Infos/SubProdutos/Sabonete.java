aaf6bdf25226d8f9e795342961759fd22229a8d7
Maya.Vitch <81265584+MayaAVitch@users.noreply.github.com>
2025-05-27 14:32:05 -0300

    Update Sabonete.java

--- src/Infos/SubProdutos/Sabonete.java
+++ src/Infos/SubProdutos/Sabonete.java
@@ -1,4 +1,12 @@
 package Infos.SubProdutos;
 
-public class Sabonete {
+import Infos.Produto;
+
+public class Sabonete extends Produto {
+    public Sabonete(String nome, int quantidade, double preco){super(nome, quantidade, preco);}
+
+    @Override
+    public void exibirInformacoes(){
+        System.out.println("O produto " + super.nome + " tem um preço de R$" + super.preco + " e uma quantidade de " + super.quantidade + " no estoque.");
+    }
 }
