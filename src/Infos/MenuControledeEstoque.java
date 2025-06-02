8964fbdf3b14747008adecc735363de7a278d7c5
Maya.Vitch <81265584+MayaAVitch@users.noreply.github.com>
2025-06-02 18:55:12 -0300

    Update MenuControledeEstoque.java

--- src/Infos/MenuControledeEstoque.java
+++ src/Infos/MenuControledeEstoque.java
@@ -1,5 +1,16 @@
 package Infos;
+import Infos.SubProdutos.Carne;
+import Infos.SubProdutos.Detergente;
+import Infos.SubProdutos.Tomate;
+import Infos.SubProdutos.Leite;
+import Infos.SubProdutos.Sabonete;
 
 public class MenuControledeEstoque {
+
+    Carne carneEstoque = new Carne("Alcatra Bovina", 20, 47.90);
+    Detergente detergenteEstoque = new Detergente("Detergente Ypê Neutro",100, 2.99);
+    Tomate tomateEstoque = new Tomate("Tomate Carmem",40, 1.89);
+    Leite leiteEstoque = new Leite("Leite CooperRita", 15, 4.99);
+    Sabonete saboneteEstoque = new Sabonete("Sabonete Francis", 45, 2.89);
 
 }
