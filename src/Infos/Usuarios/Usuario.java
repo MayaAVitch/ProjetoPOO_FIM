9f49e7fddf0c1659ad3941fea4a49ab58f04264a
Maya.Vitch <81265584+MayaAVitch@users.noreply.github.com>
2025-06-02 17:50:07 -0300

    Update Usuario.java

--- src/Infos/Usuarios/Usuario.java
+++ src/Infos/Usuarios/Usuario.java
@@ -4,12 +4,23 @@ public abstract class Usuario {
     //Variáveis
     protected String nome;
     protected int cpf;
-    protected int idade;
     protected double salario;
+    protected String cargo;
 
-    //Métodoss
+    public Usuario(String nome, String cargo, int cpf, double salario) {
+        this.nome = nome;
+        this.cargo = cargo;
+        this.cpf = cpf;
+        this.salario = salario;
+    }
+
+    //Métodos
     public void exibirInfo(){
-
+        System.out.println("Informações do Usuário: ");
+        System.out.println("Nome: " + nome);
+        System.out.println("CPF: " + cpf);
+        System.out.println("Salário: " + salario);
+        System.out.println("Cargo: " + cargo);
     }
 
     public void vender(){
@@ -18,5 +29,21 @@ public abstract class Usuario {
 
     public void verificarEstoque(){
 
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public String getCargo() {
+        return cargo;
     }
 }
