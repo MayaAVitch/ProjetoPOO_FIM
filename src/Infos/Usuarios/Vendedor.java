8266a61f5ab32f329e2357b0745b1673ce2e593a
Maya.Vitch <81265584+MayaAVitch@users.noreply.github.com>
2025-06-02 17:49:00 -0300

    Update Vendedor.java

--- src/Infos/Usuarios/Vendedor.java
+++ src/Infos/Usuarios/Vendedor.java
@@ -1,6 +1,18 @@
 package Infos.Usuarios;
 
-public class Vendedor {
+public class Vendedor extends Usuario {
+
+    public Vendedor(String nome, String cargo, int cpf, double salario){super(nome, cargo, cpf, salario);}
+
+    @Override
+    public void exibirInfo(){
+        System.out.println("Informações do Usuário: ");
+        System.out.println("Nome: " + nome);
+        System.out.println("CPF: " + cpf);
+        System.out.println("Salário: " + salario);
+        System.out.println("Cargo: " + cargo);
+    }
+    
     //Métodos
     public void vender(){
 
