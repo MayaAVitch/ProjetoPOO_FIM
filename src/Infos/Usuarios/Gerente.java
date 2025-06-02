06e38d1ae6fc8352e9d6415dc31383ae731f83d6
Maya.Vitch <81265584+MayaAVitch@users.noreply.github.com>
2025-06-02 17:49:20 -0300

    Update Gerente.java

--- src/Infos/Usuarios/Gerente.java
+++ src/Infos/Usuarios/Gerente.java
@@ -1,7 +1,17 @@
 package Infos.Usuarios;
 
 public class Gerente extends Usuario{
-    //Métodos
+    public Gerente(String nome, String cargo, int cpf, double salario){super(nome, cargo, cpf, salario);}
+
+    @Override
+    public void exibirInfo(){
+        System.out.println("Informações do Usuário: ");
+        System.out.println("Nome: " + nome);
+        System.out.println("CPF: " + cpf);
+        System.out.println("Salário: " + salario);
+        System.out.println("Cargo: " + cargo);
+    }
+
     public void vender(){
 
     }
